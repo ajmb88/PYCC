fbdbcc7ea26a493109c987d6b2fba60f18ec06f8
AJMB <aaron.1brazier@gmail.com>
2023-03-27 20:02:37 -0400

    dice roll practice code worked on and modified.

---
+++ Data_plotting/dice_visual.py
@@ -0,0 +1,39 @@
+from plotly.graph_objs import Bar, Layout
+from plotly import offline
+
+# Create 2 D6
+from die import Die
+
+die_1 = Die()
+die_2 = Die()
+die_3 = Die()
+# Make some rolls and store the results in a list, then display results.
+results = []
+results = [(die_1.roll() * die_2.roll() * die_3.roll()) for value in range(1000)]
+
+
+# CODE ABOVE IS SIMPLER VERSION OF CODE BELOW.
+#for roll in range(100):
+ #   result = die_1.roll() * die_2.roll() * die_3.roll()
+ #   results.append(result)
+
+
+# analyze the results.
+frequencies = []
+max_results= die_1.num_sides * die_2.num_sides * die_3.num_sides
+for value in range(1, max_results+1):
+    frequency = results.count(value)
+    frequencies.append(frequency)
+
+
+print(frequencies)
+
+# Visualize the results.
+x_value = list(range(1, max_results+1))
+data = (Bar(x = x_value, y = frequencies))
+
+x_axis_config = {'title': 'Result'}
+y_axis_config = {'title': 'Frequency or Results'}
+my_layout = Layout(title="Results of rolling 3 D6 10000 times.", xaxis = x_axis_config,
+                    yaxis = y_axis_config)
+offline.plot({'data' : data, 'layout' : my_layout}, filename= 'D6_D6_D6.html')
--- Data_plotting/die_visual.py
+++ Data_plotting/die_visual.py
@@ -1,11 +1,15 @@
 from die import Die
 
 # Create a D6
+from plotly.graph_objs import Bar, Layout
+from plotly import offline
+
+
 die = Die()
 
 # Make some rolls and store the results in a list, then display results.
 results = []
-for roll in range(100):
+for roll in range(10000):
     result = die.roll()
     results.append(result)
 
@@ -16,5 +20,15 @@ for value in range(1, die.num_sides+1):
     frequency = results.count(value)
     frequencies.append(frequency)
 
-print(results)
+
 print(frequencies)
+
+# Visualize the results.
+x_value = list(range(1, die.num_sides+1))
+data = (Bar(x = x_value, y = frequencies))
+
+x_axis_config = {'title': 'Result'}
+y_axis_config = {'title': 'Frequency or Results'}
+my_layout = Layout(title="Results of rolling one D6 10000 times.", xaxis = x_axis_config,
+                    yaxis = y_axis_config)
+offline.plot({'data' : data, 'layout' : my_layout}, filename= 'D6.html')
