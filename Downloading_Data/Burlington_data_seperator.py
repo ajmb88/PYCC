8f0947354db11cbc8fdc39e56ede3dacb96a0744
AJMB <aaron.1brazier@gmail.com>
2023-03-30 20:45:52 -0400

    no change.

--- Downloading_Data/Burlington_data_seperator.py
+++ Downloading_Data/Burlington_data_seperator.py
@@ -2,14 +2,15 @@ from datetime import datetime
 import matplotlib.pyplot as plt
 import csv
 
-filename = 'Downloading_Data/data/Burlington_percip_data_2012-2023.csv'
+filename = 'Downloading_Data/data/Burlington__temp_wind_data_1992-2023.csv'
+date_index = []
+dates = []
 
 # Opening file and creating empty lists
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
-    date_index = []
-    dates = []
+
 
     # Code to visualize the header.
     for value, item in enumerate(header_row):
@@ -22,7 +23,7 @@ with open(filename) as f:
     first_line = next(reader)
     date = first_line[date_index[0]]
     dates.append(date)
-    print(date[0:7])
+    print(date[0:4])
     print(dates)
     
     # using the last date to check against incoming dates so no duplicates are stored in the dates list
@@ -30,11 +31,34 @@ with open(filename) as f:
     for row in reader:
         last_date = dates[-1]
         new_date = row[date_index[0]]
-        if new_date[0:7] in last_date[0:7]:
+        if new_date[0:4] in last_date[0:4]:
             print('yes')
             pass
         else:
             dates.append(new_date)
     
-    print(dates)
+print(dates)
+
+
+
+
+# Date were working with for the moment is 'working_date'. Its the last dates from dates list.
+for date in dates:
+    working_date = dates.pop()
     
+    # Opening the new filename in write mode and putting the data year in title.
+    with open(f"Downloading_Data/data/Burlington_Temperature_data/Burlington_Weather_Data_Year_{working_date}.csv", 'w') as g:
+        writer = csv.writer(g)
+        writer.writerow(header_row)   
+
+        # Opening  the data file taking out the header
+        with open(filename) as f:
+            reader2 = csv.reader(f)
+            header_row = next(reader2)
+
+            # go through data until the selected date matches the date in file. Copy all info into new file.
+            for next_row in reader2:
+               
+                data_date = next_row[date_index[0]]
+                if working_date[0:4] in data_date[0:4]:
+                   writer.writerow(next_row)
