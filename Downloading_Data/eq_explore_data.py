f62369376308fce8b8e8d36e61148fe5c4a424f5
AJMB <aaron.1brazier@gmail.com>
2023-04-03 20:10:14 -0400

    Json weathermapping practice code complete.

---
+++ Downloading_Data/eq_explore_data.py
@@ -0,0 +1,24 @@
+import json
+
+# Explore the structure of the data.
+filename = 'Downloading_Data/data/eq_data_1_day_m1.json'
+with open(filename) as f:
+    all_eq_data = json.load(f)
+
+readable_file = 'Downloading_Data/data/New_Json_Files/readable_eq_data.json'
+with open(readable_file, 'w') as f:
+    json.dump(all_eq_data, f, indent=4)
+
+
+all_eq_dicts = all_eq_data['features']
+
+mags, lons, lats= [], [], []
+for eq_dict in all_eq_dicts:
+    mag = eq_dict['properties']['mag']
+    lon = eq_dict['geometry']['coordinates'][0]
+    lat = eq_dict['geometry']['coordinates'][1]
+    mags.append(mag)
+    lons.append(lon)
+    lats.append(lat)
+
+print(mags[:5],lons[:5],lats[:5])
