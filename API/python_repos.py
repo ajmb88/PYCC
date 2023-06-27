971a54fb1605e5f53eae387a732dc6ea657dc77e
ajmb88 <aaron.1brazier@gmail.com>
2023-06-27 17:14:02 -0400

    API cocde added to show top item on Firebaseio website.

--- API/python_repos.py
+++ API/python_repos.py
@@ -8,6 +8,21 @@ print(f"Status code: {r.status_code}")
 
 # Store API response in a variable.
 response_dict = r.json()
+print(f"Total Repositories: {response_dict['total_count']}")
 
-# Process results.
-print(response_dict.keys())
+# Explore information about the repositories.
+repo_dicts = response_dict['items']
+print(f"Repositories Returned: {len(repo_dicts)}")
+
+# Examine the first repositories.
+repo_dict = repo_dicts[0]
+
+for repo in repo_dict:
+    print("\nSelected information about first repository:")
+    print(f"Name: {repo_dict['name']}")
+    print(f"Owner: {repo_dict['owner']['login']}")
+    print(f"Stars: {repo_dict['stargazers_count']}")
+    print(f"Respository: {repo_dict['html_url']}")
+    print(f"Created: {repo_dict['created_at']}")
+    print(f"Updated: {repo_dict['updated_at']}")
+    print(f"Description: {repo_dict['description']}")
